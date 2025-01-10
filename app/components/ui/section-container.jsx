69f7109df45096b4867639f05f532916e83e80fa
lamtantai <108776597+lamtantai@users.noreply.github.com>
2025-01-10 09:50:00 +0700

    minor change

--- app/components/ui/section-container.jsx
+++ app/components/ui/section-container.jsx
@@ -4,13 +4,7 @@ import SplitTextAnimated from "./split-text-animated";
 export default function SectionContainer({ title, id, children }) {
   return (
     <section id={id} className="pt-section-vertical-padding">
-      <h4
-        className="mb-10 font-sans text-[clamp(3.5rem,1.3181rem+9.5645vw,10.5rem)] font-black uppercase tracking-wide text-light-grey lg:mb-20"
-        style={{
-          textShadow:
-            "0 0 2px black, 2px 2px 0px black, -2px -2px 0px black, 2px -2px 0px black, -2px 2px 0px black",
-        }}
-      >
+      <h4 className="mb-10 font-sans text-[clamp(3.5rem,1.3181rem+9.5645vw,10.5rem)] font-black uppercase tracking-wide text-black lg:mb-20">
         <SplitTextAnimated splitType="chars">{title}</SplitTextAnimated>
       </h4>
 
